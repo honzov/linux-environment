c24668a8fdff7c45eab5f346a64e9d4e8b3359d5
Jan Jirák <jirak.honza@seznam.cz>
2021-10-14 13:42:33 +0200

    nvim uncomplete

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,130 @@
+vim.o.termguicolors = true
+vim.o.syntax = 'on'
+vim.o.errorbells = false
+vim.o.smartcase = true
+vim.o.showmode = false
+vim.bo.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.completeopt='menuone,noinsert,noselect'
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+vim.wo.wrap = false
+
+vim.g.mapleader = ' '
+
+local key_mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(
+    mode,
+    key,
+    result,
+    {noremap = true, silent = true}
+  )
+end
+
+key_mapper('', '<up>', '<nop>')
+key_mapper('', '<down>', '<nop>')
+key_mapper('', '<left>', '<nop>')
+key_mapper('', '<right>', '<nop>')
+key_mapper('i', 'jk', '<ESC>')
+key_mapper('i', 'JK', '<ESC>')
+key_mapper('i', 'jK', '<ESC>')
+key_mapper('v', 'jk', '<ESC>')
+key_mapper('v', 'JK', '<ESC>')
+key_mapper('v', 'jK', '<ESC>')
+
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'sheerun/vim-polyglot'
+
+  -- these are optional themes but I hear good things about gloombuddy ;)
+  -- colorbuddy allows us to run the gloombuddy theme
+  use 'tjdevries/colorbuddy.nvim'
+  use 'bkegley/gloombuddy'
+  -- sneaking some formatting in here too
+  use {'prettier/vim-prettier', run = 'yarn install' }
+
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'anott03/nvim-lspinstall'
+
+  -- fuzzy finding
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/telescope.nvim'
+  use 'jremmen/vim-ripgrep'
+  end
+)
+
+
+vim.g.colors_name = 'gloombuddy'
+
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+  }
+}
+
+local lspconfig = require'lspconfig'
+local completion = require'completion'
+local function custom_on_attach(client)
+  print('Attaching to ' .. client.name)
+  completion.on_attach(client)
+end
+local default_config = {
+  on_attach = custom_on_attach,
+}
+-- setup language servers here
+lspconfig.tsserver.setup(default_config)
+
+-- lsp maps
+key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
+key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
+key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
+key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
+key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
+key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
+key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
+key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+
+-- fuzzy finding maps
+key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
+key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
+key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
