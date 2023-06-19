56060642bbb6acf7bee7f8d690418dabbb2f0296
Jan Jirák <jirak.honza@seznam.cz>
2023-06-19 22:09:11 +0200

    Fish chosen prompt

--- install_system.sh
+++ install_system.sh
@@ -1,17 +1,19 @@
 # install all utils
 source ./core_tools
 
-# change default shell
+# change default shell to fish
 chsh -s /usr/bin/fish
 set -U fish_greeting
 cp ./fish/config.fish ~/.config/fish/
-
-# enable fzf bindings from the doc
-mkdir -p ~/.config/fish/functions/
-echo fzf_key_bindings > ~/.config/fish/functions/fish_user_key_bindings.fish
+fish_config prompt choose informative_vcs
 
 # fisher
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install jethrokuan/z
 fisher install sei40kr/fish-ranger-cd
+
+# enable fzf bindings from the doc
+mkdir -p ~/.config/fish/functions/
+echo fzf_key_bindings > ~/.config/fish/functions/fish_user_key_bindings.fish
+
 
